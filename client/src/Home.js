aecec460f2c0c6cac7efe0c1040ff838d1a088b2
isabellehuangg <huang.yuqing.issy@gmail.com>
2023-07-24 01:26:15 -0400

    Fixed autocomplete ingredient options

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
-import searchBar from "./Search";
+import SearchBar from "./Search";
 
 const Home = () => {
     return (  
         <div className="home">
             <h2 className="title">Hyrule Bakery</h2>
             <p className="description">Predict your next dessert, bookmark favourite recipes</p>
-            <searchBar />
+            <SearchBar />
         </div>
     );
 }
--- client/src/Search.js
+++ client/src/Search.js
@@ -1,16 +1,19 @@
-const searchBar = () => {
-    const ingredients = ['Wheat', 'Sugar', 'Apple'];
+import React, { useState, useEffect } from "react";
+
+const SearchBar = () => {
+    const ingredients = ['Wheat', 'Sugar', 'Apple']; // Sample ingredients
     const [input, setInput] = useState('');
     const [autoOptions, setAutoOptions] = useState([]); // Auto options: all possible verified options in input
 
-    // Function for onChange event
+    // Function for onChange event, search input
     const handleOnChange = (e) => {
-        const userInput = e.target.trim().toLowerCase(); 
+        const userInput = e.target.value.toLowerCase(); 
         setInput(userInput);
         const matchingIngredients = ingredients.filter((ingredient) => {
-            ingredient.startsWith(userInput);
+            return ingredient.toLowerCase().startsWith(userInput);
         });
         setAutoOptions(matchingIngredients);
+        console.log(autoOptions.length);
     };
 
     // Handle choosing one of the matching ingredients (selected: ingredient from handleOnChange)
@@ -20,8 +23,22 @@ const searchBar = () => {
     };
 
     return (  
-        <input className="search" type="text" placeholder="Search (e.g. Sugar Cane)" />
+        <div className="searchComponent">
+            <div className="searchBar">
+                <input className="search" type="text" placeholder="Search (e.g. Sugar Cane)" onChange={ handleOnChange } />
+            </div>
+            {
+                // Display options as buttons under search bar when autocomplete options length is greater than 0
+                autoOptions.length > 0 && (
+                    <div className="optionButtons">
+                        {autoOptions.map((option) => (
+                            <div key = { option } className="option" onClick={ () => chooseOne(option) }>{ option }</div>
+                        ))}
+                    </div>
+                )
+            }
+        </div>
     );
 }
  
-export default searchBar;
+export default SearchBar;
