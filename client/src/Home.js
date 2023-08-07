584b16f7234f654295cd91ff764b4f15d67a5db1
isabellehuangg <huang.yuqing.issy@gmail.com>
2023-08-06 22:41:55 -0400

    Moved searchbar to individual component

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,75 +1,11 @@
-import { useState, useEffect } from "react";
+import SearchBar from "./SearchBar";
 
 const Home = () => {
-    const ingredients = ['Wheat', 'Sugar', 'Apple']; // Sample ingredients
-    const [input, setInput] = useState('');
-    const [autoIngredients, setAutoIngredients] = useState([]);
-    const [selectedIngredients, setSelectedIngredients] = useState([]); 
-
-    const handleInput = (e) => {
-        const userInput = e.target.value.toLowerCase();
-        setInput(userInput);
-        const matchingIngredients = ingredients.filter((ingredient) => {
-            return ingredient.toLowerCase().startsWith(userInput);
-        });
-        setAutoIngredients(matchingIngredients);
-    };
-
-    const chooseOne = (ingredient) => {
-        setInput('');
-        setAutoIngredients([]);
-        setSelectedIngredients((pastIngredients) => [...pastIngredients, ingredient]);
-    };
-
-    const removeOption = (indexToRemove) => {
-        setSelectedIngredients((pastIngredients) =>
-            pastIngredients.filter((_, index) => index !== indexToRemove)
-        );
-    };
-
-    const displaySelectedIngredients = () => {
-        // index is provided from the map function
-        return selectedIngredients.map((ingredient, index) => (
-            <div key={index} className="selectedIngredient">
-                {ingredient}
-                <button onClick={() => removeOption(index)} className="closeButton">X</button>
-            </div>
-        ));
-    };
-
-    const placeHolder = () => {
-        if (selectedIngredients.length < 5) {
-            return 'Search...';
-        } else {
-            return '';
-        }
-    };
-
     return (  
         <div className="home">
             <h2 className="title">Hyrule Bakery</h2>
             <p className="description">Predict your next dessert, bookmark favorite recipes</p>
-            <div className="searchBar">
-                {displaySelectedIngredients()}
-                {
-                    selectedIngredients.length < 5 && (
-                        <input className="search" type="text" placeholder={placeHolder()} value={input} onChange={handleInput} />
-                    )
-                }
-            </div>
-            {
-                // Display Ingredients as buttons under the search bar when Ingredients length < 6
-                // value = {input} allows for search bar to empty when chooseOne is called
-                selectedIngredients.length < 5 && (
-                    <div className="optionButtons">
-                        {autoIngredients.map((option) => (
-                            <div key={option} className="option" onClick={() => chooseOne(option)}>
-                                {option}
-                            </div>
-                        ))}
-                    </div>
-                )
-            }
+            <SearchBar />
         </div>
     );
 }
---
+++ client/src/SearchBar.js
@@ -0,0 +1,82 @@
+import { useState, useEffect } from "react";
+
+const SearchBar = () => {
+    const ingredients = ['Wheat', 'Sugar', 'Apple', 'Sweetenerrrrrrrr', 'Coffee']; // Sample ingredients
+    const [input, setInput] = useState('');
+    const [autoIngredients, setAutoIngredients] = useState([]);
+    const [selectedIngredients, setSelectedIngredients] = useState([]); 
+
+    const handleInput = (e) => {
+        const userInput = e.target.value.toLowerCase();
+        setInput(userInput);
+        const matchingIngredients = ingredients.filter((ingredient) => {
+            return ingredient.toLowerCase().startsWith(userInput);
+        });
+        setAutoIngredients(matchingIngredients);
+    };
+
+    const chooseOne = (ingredient) => {
+        setInput('');
+        setAutoIngredients([]);
+        setSelectedIngredients((pastIngredients) => [...pastIngredients, ingredient]);
+    };
+
+    const removeOption = (indexToRemove) => {
+        setSelectedIngredients((pastIngredients) =>
+            pastIngredients.filter((_, index) => index !== indexToRemove)
+        );
+    };
+
+    const displaySelectedIngredients = () => {
+        // index is provided from the map function
+        return selectedIngredients.map((ingredient, index) => (
+            <div key={index} className="selectedIngredient">
+                {ingredient}
+                <button onClick={() => removeOption(index)} className="closeButton">X</button>
+            </div>
+        ));
+    };
+
+    const handleEnter = (e) => {
+        if (e.key === 'Enter' && autoIngredients.length > 0 && input.length > 0) {
+            chooseOne(autoIngredients[0]);
+        }
+    }
+
+    const placeHolder = () => {
+        if (selectedIngredients.length < 5) {
+            return 'Search...';
+        } else {
+            return '';
+        }
+    };
+
+    return ( 
+        <div className="search" onKeyDown={handleEnter}>
+            <div className="searchBar">
+                {displaySelectedIngredients()}
+                {
+                    selectedIngredients.length < 5 && (
+                        <input className="search" type="text" placeholder={placeHolder()} value={input} onChange={handleInput} />
+                    )
+                }
+            </div>
+            {
+                // Display Ingredients as buttons under the search bar when Ingredients length < 6
+                // value = {input} allows for search bar to empty when chooseOne is called
+                selectedIngredients.length < 5 && (
+                    <div className="optionButtons">
+                        {input.length > 0 && <p className="matching">Matching Ingredients: </p>}
+                        {input.length > 0 && (autoIngredients.map((option) => (
+                            <div key={option} className="option" onClick={() => chooseOne(option)}>
+                                {option}
+                            </div>
+                        )))}
+                    </div>
+                )
+            }
+        </div>
+     );
+}
+ 
+export default SearchBar;
