e6c5ed12c33d173b9ddb39ec74c1508b79cef6aa
isabellehuangg <huang.yuqing.issy@gmail.com>
2023-09-07 17:21:20 -0400

    CSS to home img

--- client/src/Home.js
+++ client/src/Home.js
@@ -4,7 +4,7 @@ import Zelda from "./zelda.png"
 const Home = () => {
     return (  
         <div className="home">
-            <img src={Zelda} className="title"/>
+            <img src={Zelda} className="title2"/>
             <p className="description">Find your next dessert to bake in TOTK!</p>
             <SearchBar />
         </div>
